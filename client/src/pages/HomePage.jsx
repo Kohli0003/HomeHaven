b6e9b962e2cee0ded57d2fd5cf0a53e16cbe3396
Kohli0003 <aakarshitkohli26@gmail.com>
2024-05-06 13:56:26 +0530

    commit

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/Navbar";
+import Slide from "../components/Slide";
+import Categories from "../components/Categories";
+import Listings from "../components/Listings";
+import Footer from "../components/Footer";
+
+const HomePage = () => {
+  return (
+    <>
+      <Slide />
+      <Categories />
+      <Listings />
+    </>
+  );
+};
+
+export default HomePage;
